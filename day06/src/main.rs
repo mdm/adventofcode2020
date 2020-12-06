c12b4699015ae60694a03c09c387d68963624453
Marc Dominik Migge <marcmigge@gmx.net>
2020-12-06 14:14:02 +0100

    Finish day 6

---
+++ day06/src/main.rs
@@ -0,0 +1,42 @@
+use std::{io::BufRead};
+use std::collections::HashSet;
+
+fn main() {
+    let filename = std::env::args().nth(1).unwrap();
+    let file = std::fs::File::open(filename).unwrap();
+    let reader = std::io::BufReader::new(file);
+
+    let all_chars: Vec<_> = "abcdefghijklmnopqrstuvwxyz".chars().collect();
+
+    let mut groups_one = Vec::new();
+    let mut groups_all = Vec::new();
+    let mut group_one = HashSet::new();
+    let mut group_all = all_chars.clone();
+    for line in reader.lines() {
+        let line = line.unwrap();
+
+        if line.is_empty() {
+            groups_one.push(group_one);
+            groups_all.push(group_all);
+
+            group_one = HashSet::new();
+            group_all = all_chars.clone();
+
+            continue;
+        }
+
+        for character in line.chars() {
+            group_one.insert(character);
+        }
+
+        group_all = group_all.into_iter().filter(|character| line.contains(*character)).collect();
+    }
+    groups_one.push(group_one);
+    groups_all.push(group_all);
+
+    let answer_part_1: usize = groups_one.iter().map(|group| group.len()).sum();
+    println!("{}", answer_part_1);
+
+    let answer_part_2: usize = groups_all.iter().map(|group| group.len()).sum();
+    println!("{}", answer_part_2);
+}
